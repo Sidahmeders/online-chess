e952ff842f31b53ce74f6eb59671ff03fb59f54d
Sidahmeders <ahorob57@gmail.com>
2022-03-27 12:37:54 +0200

    laied off all the pieces

--- scripts/board.js
+++ scripts/board.js
@@ -12,8 +12,8 @@ const styles = {
     display: flex;
   `,
   chessPieceStyle: `
-    width: 100%;
-    height: 100%;
+    width: 90%;
+    height: 90%;
   `,
 }
 
@@ -54,10 +54,18 @@ function chessPiece(i, j) {
   iconElement.style = styles.chessPieceStyle
 
   if (i === 1) iconElement.src = './icons/P-B.png'
+  if (i === 0 && j === 4) iconElement.src = './icons/Q-B.png'
+  if (i === 0 && j === 5) iconElement.src = './icons/K-B.png'
   if ((i === 0 && j === 1) || (i === 0 && j === 8)) iconElement.src = './icons/R-B.png'
+  if ((i === 0 && j === 2) || (i === 0 && j === 7)) iconElement.src = './icons/N-B.png'
+  if ((i === 0 && j === 3) || (i === 0 && j === 6)) iconElement.src = './icons/B-B.png'
 
   if (i === 6) iconElement.src = './icons/P-W.png'
+  if (i === 7 && j === 4) iconElement.src = './icons/Q-W.png'
+  if (i === 7 && j === 5) iconElement.src = './icons/K-W.png'
   if ((i === 7 && j === 1) || (i === 7 && j === 8)) iconElement.src = './icons/R-W.png'
+  if ((i === 7 && j === 2) || (i === 7 && j === 7)) iconElement.src = './icons/N-W.png'
+  if ((i === 7 && j === 3) || (i === 7 && j === 6)) iconElement.src = './icons/B-W.png'
 
   return iconElement
 }
