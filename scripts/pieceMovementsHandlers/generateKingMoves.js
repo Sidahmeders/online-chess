ce710517b232e8e80b8259a88b85706fdc7b83ea
Sidahmeders <ahorob57@gmail.com>
2022-06-11 18:50:48 +0100

    refactored the genratePawnMoves handler

--- scripts/pieceMovementsHandlers/generateKingMoves.js
+++ scripts/pieceMovementsHandlers/generateKingMoves.js
@@ -1,7 +1,6 @@
-import { PIECES } from '../setup.js'
 import { _state, getPieceColor, highlightPieceMovements } from './validateMoves.js'
 
-export default function generatePawnMoves({ boardArray, pieceMoves, selectedNode }) {
+export default function generateKingMoves({ boardArray, pieceMoves, selectedNode }) {
   // console.log(boardArray, virtualBoardArray)
   _state.validMoves = []
   const visitedDirections = new Set()
@@ -10,22 +9,13 @@ export default function generatePawnMoves({ boardArray, pieceMoves, selectedNode
     while (!visitedDirections.has(key)) {
       const targetNode = boardArray[positionCounter]
       const pieceColor = getPieceColor(selectedNode.id)
-      const targetPieceColor = getPieceColor(targetNode.id)
+      // const targetPieceColor = getPieceColor(targetNode.id)
 
       const nodePosition = selectedNode.getAttribute('position')
       const targetNodePosition = targetNode.getAttribute('position')
-      const canCapture = pieceColor !== targetPieceColor && targetPieceColor !== 'Empty' && key !== 'up'
 
-      const isFriendlPiece = nodePosition !== targetNodePosition && targetPieceColor !== 'Empty'
-      // if we found a friendlly piece, stop explorig and return
-      if (isFriendlPiece && !canCapture) return
-
-      const [nodeRow, targetRow] = [Math.floor(nodePosition / 8), Math.floor(targetNodePosition / 8)]
-      const canMoveForward = parseInt(targetNode.id) === PIECES.Empty && key === 'up'
-      const canLeapTo = Math.abs(nodeRow - targetRow) === 1
-
-      // if we found an opposing piece, capture it and return
-      if ((canCapture || canMoveForward) && canLeapTo) {
+      if (pieceColor) {
+        console.log(targetNode)
         highlightPieceMovements(targetNode)
         visitedDirections.add(key)
         const validMove = pieceColor === 'White' ? val + parseInt(nodePosition) : parseInt(targetNodePosition)
--- scripts/pieceMovementsHandlers/validateMoves.js
+++ scripts/pieceMovementsHandlers/validateMoves.js
@@ -1,24 +1,27 @@
 import { PIECES, getPieceTypeFromNumber, PIECES_MOVES } from '../setup.js'
 import generatePawnMoves from './generatePawnMoves.js'
+import generateKingMoves from './generateKingMoves.js'
 
 export const _state = {
   highlightedBoardSquares: [],
   validMoves: null,
-  playingTurn: null,
 }
 
 export default function validateMoves(selectedNode) {
   const boardContainer = document.getElementById('board-container')
-  const { boardArray, virtualBoardArray } = createVirtualBoard(boardContainer)
-  const piecePosition = selectedNode.getAttribute('position')
+  const { boardArray, virtualBoardArray } = createBoardArrays(boardContainer)
   const pieceType = getPieceTypeFromNumber(selectedNode.id)
   const pieceMoves = PIECES_MOVES()[pieceType]
 
-  const pieceDAO = { boardArray, virtualBoardArray, piecePosition, pieceMoves, selectedNode }
+  const pieceDAO = { boardArray, virtualBoardArray, pieceMoves, selectedNode }
   /* eslint-disable indent */
+
   switch (pieceType) {
     case 'Pawn':
       generatePawnMoves(pieceDAO)
+      break
+    case 'King':
+      generateKingMoves(pieceDAO)
       break
     default:
       console.warn('unhanlded condition')
@@ -26,7 +29,7 @@ export default function validateMoves(selectedNode) {
   }
 }
 
-function createVirtualBoard(boardContainer) {
+function createBoardArrays(boardContainer) {
   const boardArray = []
   const virtualBoardArray = []
   boardContainer.childNodes.forEach((rankNode, i) => {
@@ -48,9 +51,9 @@ export function getPieceColor(pieceID) {
   if (pieceID > 16) return 'Black'
 }
 
-export function highlightPieceMovements(boardColumnElement) {
-  boardColumnElement.style.background = '#49dd'
-  _state.highlightedBoardSquares.push(boardColumnElement)
+export function highlightPieceMovements(targetNode) {
+  targetNode.style.background = '#49dd'
+  _state.highlightedBoardSquares.push(targetNode)
 }
 
 export function clearPieceMovementsHighlight() {
@@ -59,14 +62,10 @@ export function clearPieceMovementsHighlight() {
 }
 
 export function swapNodes(selectedNode, targetNode) {
-  const [selected, target] = [selectedNode.getAttribute('position'), targetNode.getAttribute('position')]
-  const isValidMove =
-    _state.playingTurn === 'White'
-      ? _state.validMoves.some((val) => val + parseInt(selected) == parseInt(target))
-      : _state.validMoves.some((val) => val + parseInt(target) == parseInt(selected))
-
+  const targetPosition = parseInt(targetNode.getAttribute('position'))
+  const canSwapNodes = _state.validMoves.includes(targetPosition)
   _state.validMoves = []
-  if (!isValidMove) return
+  if (!canSwapNodes) return
 
   targetNode.src = selectedNode.src
   targetNode.id = selectedNode.id
