2937fee22a2ca52c89eb93080d124347c5591d8b
Sidahmeders <ahorob57@gmail.com>
2022-06-12 16:13:08 +0100

    refactored the generatePawnmoves hnalder and removed some uncesseary code

--- scripts/pieceMovementsHandlers/generatePawnMoves.js
+++ scripts/pieceMovementsHandlers/generatePawnMoves.js
@@ -6,38 +6,23 @@ export default function generatePawnMoves({ boardArray, pieceMoves, selectedNode
   _state.validMoves = []
 
   Object.entries(pieceMoves).forEach(([key, val]) => {
-    let positionCounter = parseInt(selectedNode.getAttribute('position'))
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      const targetNode = boardArray[positionCounter]
+    const nodePosition = parseInt(selectedNode.getAttribute('position'))
+    const pieceColor = getPieceColor(selectedNode.id)
 
-      const pieceColor = getPieceColor(selectedNode.id)
-      const targetPieceColor = getPieceColor(targetNode.id)
+    const targetIndex = pieceColor === 'White' ? nodePosition + val : nodePosition - val
+    if (targetIndex < 0 || targetIndex > 63) return
 
-      const nodePosition = selectedNode.getAttribute('position')
-      const targetNodePosition = targetNode.getAttribute('position')
+    const targetNode = boardArray[targetIndex]
+    const targetNodePosition = parseInt(targetNode.getAttribute('position'))
+    const targetPieceColor = getPieceColor(targetNode.id)
 
-      const canCapture = pieceColor !== targetPieceColor && targetPieceColor !== 'Empty' && key !== 'up'
-      const isFriendlPiece = nodePosition !== targetNodePosition && targetPieceColor !== 'Empty'
+    const canCapture = pieceColor !== targetPieceColor && targetPieceColor !== 'Empty' && key !== 'up'
+    const canMoveForward = parseInt(targetNode.id) === PIECES.Empty && key === 'up'
 
-      // if we found a friendlly piece, stop explorig and return
-      if (isFriendlPiece && !canCapture) return
-
-      const [nodeRow, targetRow] = [Math.floor(nodePosition / 8), Math.floor(targetNodePosition / 8)]
-      const canMoveForward = parseInt(targetNode.id) === PIECES.Empty && key === 'up'
-      const canLeapTo = Math.abs(nodeRow - targetRow) === 1
-
-      // if we found an opposing piece, capture it and return
-      if ((canCapture || canMoveForward) && canLeapTo) {
-        const validMove = pieceColor === 'White' ? val + parseInt(nodePosition) : parseInt(targetNodePosition)
-        highlightPieceMovements(boardArray[validMove])
-        _state.validMoves.push(validMove)
-      }
-
-      if (pieceColor === 'White') positionCounter += val
-      if (pieceColor === 'Black') positionCounter -= val
-      // if we get out of bound, return
-      if (positionCounter < 0 || positionCounter > 63) return
+    if (canCapture || canMoveForward) {
+      const validMove = pieceColor === 'White' ? nodePosition + val : targetNodePosition
+      highlightPieceMovements(boardArray[validMove])
+      _state.validMoves.push(validMove)
     }
   })
 }
