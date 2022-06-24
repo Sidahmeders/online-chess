703e4b68488aa86d6275bf3a65ccd20015030f53
Sidahmeders <ahorob57@gmail.com>
2022-06-24 13:12:17 +0100

    hanlded the fenParsing and loading to the dom-board

--- scripts/loadPositionFromFen.js
+++ scripts/loadPositionFromFen.js
@@ -4,36 +4,40 @@ import { FEN_STARTING_POSITION, PIECES, PieceTypeFromSymbol } from './setup.js'
 export default function loadPositionFromFen() {
   // rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
   const fenBoard =
-    'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR' ||
-    'rkbqk1kr/pppp1ppp/4p3/2b5/8/4PN2/PPPP1PPP/RKBQKB1R' ||
-    FEN_STARTING_POSITION.split(' ')[0]
-  const boardContainer = document.getElementById('board-container')
+    // 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR' ||
+    'rnbqk1nr/pppp1ppp/4p3/2b5/8/4PN2/PPPP1PPP/RNBQKB1R' || FEN_STARTING_POSITION.split(' ')[0]
+  const boardNodes = document.getElementById('board-container').childNodes
 
   let positionCounter = 0
-  boardContainer.childNodes.forEach((rankNode, i) => {
-    const row = fenBoard.split('/')[i]?.split('') || []
-    const fileNodes = rankNode.childNodes
+  fenBoard.split('/').forEach((fenRow, i) => {
+    const boardRanks = transformFenArray(fenRow)
+    const fileNodes = boardNodes[i].childNodes
 
-    // console.log(row)
-
-    for (let j = 0; j < fileNodes.length; j++) {
-      const node = fileNodes[j]
-
+    fileNodes.forEach((node, j) => {
       const isBoardBound = i < 8 && j > 0
       if (isBoardBound) {
         const isBlackTile = (i + j) % 2
         node.style.background = isBlackTile ? '#0606' : '#ff66'
         node.setAttribute('tile-color', isBlackTile ? 'black' : 'white')
         node.setAttribute('row-index', i)
-
-        const pieceEntry = row[j - 1]?.toLocaleLowerCase()
-        const pieceColor = row[j - 1] === pieceEntry ? PIECES.Black : PIECES.White
+        const pieceEntry = boardRanks[j - 1]?.toLocaleLowerCase()
+        const pieceColor = boardRanks[j - 1] === pieceEntry ? PIECES.Black : PIECES.White
         const boardPiece = PieceTypeFromSymbol[pieceEntry] + pieceColor
-
         const pieceID = boardPiece ? boardPiece : PIECES.Empty
         const IconElement = makePiece(pieceID, positionCounter++)
         node.appendChild(IconElement)
       }
-    }
+    })
   })
+}
+
+function transformFenArray(fenRow) {
+  const boardRanks = []
+  for (let i = 0; i < fenRow.length; i++) {
+    const tmpColumn = fenRow[i]
+    const fenChar = parseInt(tmpColumn)
+    if (fenChar) for (let j = 0; j < fenChar; j++) boardRanks.push('1')
+    else boardRanks.push(tmpColumn)
+  }
+  return boardRanks
 }
